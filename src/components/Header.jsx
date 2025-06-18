f7173bdad2f8ba131a43bdc4c359d68bea6fa58c
Md Aman <aman.md0109@gmail.com>
2025-06-18 15:11:55 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header=()=>
                 <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-                <li ><Link to="/cart">Cart</Link></li>
+                <li ><Link to="/cart">Carrt</Link></li>
                 <li ><Link to="/groceries">Groceries</Link></li>
                 <button className="Login-btn"
                 onClick={()=>
